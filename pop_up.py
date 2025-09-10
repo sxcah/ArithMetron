5f77aa4aff1a80ea14d8e6cea37141d31a3c0768
MikkoHacxs <smgesulgon@usa.edu.ph>
2025-09-11 03:35:24 +0800

    pwer ups init commit

--- pop_up.py
+++ pop_up.py
@@ -85,17 +85,6 @@ class SettingsPopup():
             except py.error as e:
                 print(f"Warning: Could not load game music: {e}")
 
-    def play_boss_music(self):
-        if self.music_enabled and boss_bgm:
-            try:
-                if self.current_music != "boss":
-                    py.mixer.music.load(boss_bgm)
-                    py.mixer.music.play(loops=-1)
-                    self.current_music = "boss"
-                py.mixer.music.set_volume(self.music_volume)
-            except py.error as e:
-                print(f"Warning: Could not load boss music: {e}")
-
     def stop_music(self):
         py.mixer.music.stop()
         self.current_music = None
